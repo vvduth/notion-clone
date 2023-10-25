f0683e3b129ed3b36847ee4f541ee1270b62781f
Duc Thai <ducthai060501@gmail.com>
2023-10-25 14:52:20 +0300

    authentication with clerk and convex backend set up

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -3,8 +3,12 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import React from "react";
-
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import Spinner from "@/components/Spinner";
+import Link from "next/link";
 const Heading = () => {
+  const { isLoading, isAuthenticated } = useConvexAuth();
   return (
     <div className="max-w-3xl space-y-4 ">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -15,10 +19,28 @@ const Heading = () => {
         Notion Clone to the connect workspace where <br />
         better, faster work happens.
       </h3>
-      <Button>
-        Enter Notion Clone
-        <ArrowRight className="h-4 w-4 ml-2"/>
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size={"lg"} />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href={"/documents"}>
+            Enter Notion Clone
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get it free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { useScrollTop } from "@/hooks/use-scroll-top";
+import { useConvexAuth } from "convex/react";
 import { cn } from "@/lib/utils";
 import React from "react";
 import Logo from "./Logo";
 import { ModeToggle } from "@/components/mode-toggle";
-
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import Spinner from "@/components/Spinner";
+import Link from "next/link";
 const Navbar = () => {
+  const { isLoading, isAuthenticated } = useConvexAuth();
   const scrolled = useScrollTop();
   return (
     <div
@@ -16,9 +21,34 @@ const Navbar = () => {
       )}
     >
       <Logo />
-      <div className="md:ml-auto 
+      <div
+        className="md:ml-auto 
       md:justify-end justify-between 
-      w-full flex items-center gap-x-2">
+      w-full flex items-center gap-x-2"
+      >
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode={"modal"}>
+              <Button variant={"ghost"} size={"sm"}>
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode={"modal"}>
+              <Button size={"sm"}>Get it free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant={"ghost"} size={"sm"} asChild>
+              <Link href={"/documents"}>
+                Enter this app
+              </Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
